f0e44087192c7582d4c65b3d30f9fa7b0969a7bb
Dev-MyFamily <beniciosantos004@gmail.com>
2024-11-13 11:41:51 -0300

    Update MothersDay.tsx

--- src/components/MothersDay.tsx
+++ src/components/MothersDay.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Moon, Sun, Upload, Heart, ChevronLeft, ChevronRight } from 'lucide-react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid } from 'recharts';
 
